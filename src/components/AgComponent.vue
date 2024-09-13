be31145cc8e1b482f0b857a93859cf3afe626854
xyztai <taixyz@163.com>
2024-09-13 13:08:45 +0800

    update axios

--- src/components/AgComponent.vue
+++ src/components/AgComponent.vue
@@ -2,28 +2,29 @@
   <div>
     <h1>Ag</h1>
     <ul v-if="cnts.length">
-      <li v-for="detail in cnts" :key="detail.time">{{ detail.time }}</li>
+      <li v-for="detail in cnts" :key="detail.time">{{ detail.time }} {{ detail.cntGap }} {{ detail.cntCalc }} {{ detail.cntCp }} {{ detail.namesMiss }}</li>
     </ul>
     <p v-else-if="error">{{ error }}</p>
     <p v-else>Loading data...</p>
-    <button @click="fetchCnt">Refresh Data</button>
+    <button @click="fetchCnts">Refresh Data</button>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:8081'
+// axios.defaults.baseURL = 'http://localhost:8081'
 
 export default {
   setup() {
-    var cnts = ref([])
+    const cnts = ref([])
     const error = ref(null)
 
     const fetchCnts = async() => {
       try {
-        const response = await axios.get('/e/ag/data/cnt')
-        cnts = response.data
+        const response = await axios.get('/ag/data/cnt')
+        cnts.value = response.data.data
+        console.log(cnts)
       } catch (err) {
         error.value = 'Error Fetching cnts: ' + err.message
         console.error('Axios error:', err)
--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios'
-axios.defaults.baseURL = '/e'
-axios.defaults.proxy = {
-  host: '142.171.151.123',
-  port: 51666,
-  protocol: 'http'
-}
 
 createApp(App).mount('#app')
