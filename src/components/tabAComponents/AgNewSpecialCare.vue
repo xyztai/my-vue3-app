5ad80407d14fc6c4d55914754b8e2afba33770d9
xyztai <taixyz@163.com>
2025-11-30 21:18:00 +0800

    Update AgNewSpecialCare.vue

--- src/components/tabAComponents/AgNewSpecialCare.vue
+++ src/components/tabAComponents/AgNewSpecialCare.vue
@@ -12,8 +12,8 @@
     <el-table-column prop="date" label="日期" align="center" width="100" sortable label-class-name="time" fixed />
     <el-table-column prop="stockCode" label="名称" align="left" width="120" fixed />
     <el-table-column prop="last" label="cp" align="center" min-width="80" width="auto" :formatter="formatAmount" />
-    <el-table-column prop="ratioB" label="买(越小越买)" align="center" min-width="120" width="auto" :formatter="formatAmount" />
-    <el-table-column prop="ratioS" label="卖(越小越卖)" align="center" min-width="120" width="auto" :formatter="formatAmount" />
+    <el-table-column prop="ratioB" label="买(越小越买)" align="center" sortable min-width="120" width="auto" :formatter="formatAmount" />
+    <el-table-column prop="ratioS" label="卖(越小越卖)" align="center" sortable min-width="120" width="auto" :formatter="formatAmount" />
   </el-table>
 </template>
 
