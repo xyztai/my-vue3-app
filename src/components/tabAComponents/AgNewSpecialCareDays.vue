db24439f91034d9a6b563dca2b2ac2dd05072a81
xyztai <taixyz@163.com>
2025-12-04 05:48:54 +0800

    更新dist

--- src/components/tabAComponents/AgNewSpecialCareDays.vue
+++ src/components/tabAComponents/AgNewSpecialCareDays.vue
@@ -20,7 +20,7 @@
       <el-table-column prop="stockCode" label="名称" align="left" width="120" fixed />
       <el-table-column prop="last" label="cp" align="center" min-width="80" width="auto" :formatter="formatAmount" />
       <el-table-column prop="ratioB" label="买(<0.15)" align="center" sortable min-width="120" width="auto" :formatter="formatAmount" />
-      <el-table-column prop="ratioS" label="卖(<0.25)" align="center" sortable min-width="120" width="auto" :formatter="formatAmount" />
+      <!-- <el-table-column prop="ratioS" label="卖(<0.25)" align="center" sortable min-width="120" width="auto" :formatter="formatAmount" /> -->
     </el-table>
   </div>
 </template>
