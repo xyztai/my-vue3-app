600156b4ec2492c39c7760cae1d20392e6141a75
xyztai <taixyz@163.com>
2025-12-22 22:27:23 +0800

    更新dist

--- src/components/tabAComponents/AgNewSpecialCareDays.vue
+++ src/components/tabAComponents/AgNewSpecialCareDays.vue
@@ -75,6 +75,15 @@ export default {
           tableData.value = response.data.data
           console.log('tableData.value========', tableData.value)
         }
+        if(inputValue4.value == 3) {
+          const response = await axios.get('/ag-new/special-care-days-eastmoney-365/'
+            + (inputValue2.value === null || inputValue2.value === undefined || Object.keys(inputValue2.value).length === 0 ? '-1' : inputValue2.value) 
+            + '/' + (inputValue3.value === null || inputValue3.value === undefined || Object.keys(inputValue3.value).length === 0 ? '300' : inputValue3.value) 
+          )
+          console.log('response.data.data========', response.data.data)
+          tableData.value = response.data.data
+          console.log('tableData.value========', tableData.value)
+        }
       } catch (err) {
         error.value = 'Error Fetching cnts: ' + err.message
         console.error('Axios error:', err)
