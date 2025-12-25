6f6e2ecc485e698a306110bb396116dbed4aa15f
xyztai <taixyz@163.com>
2025-12-25 21:30:17 +0800

    更新dist

--- src/components/tabAComponents/AgNewSpecialCareDays.vue
+++ src/components/tabAComponents/AgNewSpecialCareDays.vue
@@ -84,7 +84,7 @@ export default {
       { value: 3, label: '30-365-top3' },
       { value: 4, label: '2024919-1.5%-QQ' }
     ])
-    const value2 = ref(true)
+    const value5 = ref(true)
 
     const fetchData = async() => {
       try {
