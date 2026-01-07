f3edf19caa3f79da471234b1bb899b8c82fb8749
xyztai <taixyz@163.com>
2026-01-08 07:07:38 +0800

    更新dist

--- src/components/tabAComponents/AgEastmoneyStock.vue
+++ src/components/tabAComponents/AgEastmoneyStock.vue
@@ -89,7 +89,8 @@ export default {
       { value: 2, label: 'top3-30' },
       { value: 3, label: 'volume*3' },
       { value: 4, label: 'today-volume*3' },
-      { value: 5, label: '今日数据' },
+      { value: 5, label: '9_zhuan' },
+      { value: 6, label: '今日数据' },
       // { value: 4, label: '1.5%-2020' },
       // { value: 5, label: 'QQ-1.5%-2024919' }
     ])
@@ -184,6 +185,24 @@ export default {
         }
 
         if(selectedValue.value == 5) {
+          const method = 'quer9Zhuan';
+          const key = 'stock-' + method;
+          const myData = getCachedData(key);
+          if (!myData) {
+            // 从API获取数据并缓存
+            const response = await axios.get('/ag-eastmoney-stock/' + method )
+            // console.log('response.data.data========', response.data.data)
+            tableData.value = response.data.data
+            // console.log('tableData.value========', tableData.value)          
+            cacheData(key, response.data.data)
+          } else {
+            // 使用缓存的数据
+            tableData.value = myData
+            // console.log('Using cached data:', myData);
+          }
+        }
+
+        if(selectedValue.value == 6) {
           const method = 'eastmoney-latest-info';
           const key = 'stock-' + method;
           const myData = getCachedData(key);
