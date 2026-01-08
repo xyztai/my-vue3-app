b25ea52b314f3cbe3da8649129212f4bd9c28904
xyztai <taixyz@163.com>
2026-01-08 18:25:26 +0800

    更新dist

--- src/components/tabAComponents/AgEastmoneyStock.vue
+++ src/components/tabAComponents/AgEastmoneyStock.vue
@@ -300,7 +300,7 @@ export default {
     },
     handleRowClassName(row) {
       // console.log('handleRowClassName, ', row, row.rowIndex)
-      if (row.row.date.startsWith('T+') || row.row.date.startsWith('9999')) {
+      if (row.row.date.startsWith('T+') || row.row.date.startsWith('9999') || row.row.ratioB.startsWith('B_09')) {
         return 'row-expect'
       }
       if (row.rowIndex % 2 === 1) {
