b1a53618e233a1e40cdcc1d91abdebfb9242eba3
xyztai <taixyz@163.com>
2026-01-10 22:54:45 +0800

    update

--- src/components/tabAComponents/AgEastmoneyStock.vue
+++ src/components/tabAComponents/AgEastmoneyStock.vue
@@ -45,7 +45,7 @@
       <el-table-column prop="date" label="日期" align="center" width="100" sortable label-class-name="time" fixed />
       <el-table-column prop="stockCode" label="名称" align="left" width="115" sortable fixed />
       <el-table-column prop="last" :label="'cp\nchg'" align="center" min-width="65" width="auto" :formatter="formatAmount" />
-      <el-table-column style="font-size: 8px;" prop="ratioB" :label="'e5\ne10'" align="center" min-width="65" width="auto" :formatter="formatAmount" />
+      <el-table-column prop="ratioB" :label="'e5\ne10'" align="center" min-width="65" width="auto" :formatter="formatAmount" style="font-size: 2px;" />
       <!-- <el-table-column prop="ratioS" label="卖(<0.25)" align="center" sortable min-width="120" width="auto" :formatter="formatAmount" /> -->
     </el-table>
     <div>
