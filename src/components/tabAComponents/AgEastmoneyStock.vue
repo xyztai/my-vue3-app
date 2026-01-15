a2dad6e8b75cc177181b47650490aa052d715a99
xyztai <taixyz@163.com>
2026-01-16 00:58:15 +0800

    update

--- src/components/tabAComponents/AgEastmoneyStock.vue
+++ src/components/tabAComponents/AgEastmoneyStock.vue
@@ -344,7 +344,7 @@ export default {
     },
     handleRowClassName(row) {
       // console.log('handleRowClassName, ', row, row.rowIndex)
-      if (row.row.date !== null && row.row.date !== undefined && 
+      if (row.row.date !== null && row.row.date !== undefined && row.row.ratioB !== null && row.row.ratioB !== undefined &&
           (row.row.date.startsWith('T+') || row.row.date.startsWith('9999') 
              || row.row.ratioB.startsWith('B_09U') || row.row.ratioB.startsWith('S_07U'))) {
           return 'row-expect'
