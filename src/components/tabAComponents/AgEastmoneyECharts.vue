65b0e82012fab6826e62215b26fdbddacf9aa1ff
xyztai <taixyz@163.com>
2026-01-18 18:54:14 +0800

    update

--- src/components/tabAComponents/AgEastmoneyECharts.vue
+++ src/components/tabAComponents/AgEastmoneyECharts.vue
@@ -1,60 +1,61 @@
 <template>
-  <el-row>
-    <el-col :span="24">
-      <v-chart class="chart" :options="chartOptions" autoresize />
-    </el-col>
-  </el-row>
+  <div class="chart-container" ref="chartRef"></div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
-import VChart from 'vue-echarts';
-import { use } from 'echarts/core';
-import { CanvasRenderer } from 'echarts/renderers';
-import { LineChart } from 'echarts/charts';
-import {
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  DatasetComponent,
-} from 'echarts/components';
-import 'echarts/i18n/langEN'; // 根据需要引入语言包
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import * as echarts from 'echarts'
 
-use([
-  CanvasRenderer,
-  LineChart,
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  DatasetComponent,
-]);
+const chartRef = ref(null)
+let chartInstance = null
 
-export default defineComponent({
-  components: { VChart },
-  data() {
-    return {
-      chartOptions: {
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
-          type: 'line'
-        }]
+onMounted(() => {
+  chartInstance = echarts.init(chartRef.value)
+
+  // 模拟数据
+  const data = [
+    { name: 'A', value: 100 },
+    { name: 'B', value: 200 },
+    { name: 'C', value: 150 },
+    { name: 'D', value: 300 },
+    { name: 'E', value: 88 },
+    { name: 'F', value: 98 },
+    { name: 'G', value: 123 },
+    { name: 'H', value: 219 }
+  ]
+
+  // 配置项
+  const options = {
+    xAxis: {
+      type: 'category',
+      data: data.map((item) => item.name)
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: data.map((item) => item.value),
+        type: 'line'
       }
-    };
+    ]
   }
-});
+
+  // 设置图表配置项
+  chartInstance.setOption(options)
+})
+
+onUnmounted(() => {
+  if (chartInstance) {
+    chartInstance.dispose()
+    chartInstance = null
+  }
+})
 </script>
 
 <style scoped>
-  .chart {
-    width: 100%; /* 或者具体宽度 */
-    height: 400px; /* 或者具体高度 */
-  }
+.chart-container {
+  width: 400px;
+  height: 300px;
+}
 </style>
-  
