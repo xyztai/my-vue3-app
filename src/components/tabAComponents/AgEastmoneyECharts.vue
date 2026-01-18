0d487ef4801e3863785f56b4e50271251b7a9174
xyztai <taixyz@163.com>
2026-01-18 21:48:42 +0800

    update

--- src/components/tabAComponents/AgEastmoneyECharts.vue
+++ src/components/tabAComponents/AgEastmoneyECharts.vue
@@ -11,7 +11,7 @@ export default {
   setup() {
     const chartRef = ref(null);
     let chartInstance = null;
-    const data = null;  
+    let data = null;  
     const error = ref(null)
     
     const fetchData = () => {
