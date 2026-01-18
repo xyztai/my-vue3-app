e1126561ae5dbee9d76291970c3a6a11449fbb49
xyztai <taixyz@163.com>
2026-01-19 00:09:22 +0800

    update

--- src/components/tabAComponents/AgEastmoneyECharts.vue
+++ src/components/tabAComponents/AgEastmoneyECharts.vue
@@ -89,7 +89,7 @@ onMounted(async () => {
   const options = {
     title: {
         // text: 'B_06 ~ B_09 走势', // 图表的标题内容
-        subtext: 'B_06 ~ B_09 走势', // 图表的副标题，可选
+        // subtext: 'B_06 ~ B_09 走势', // 图表的副标题，可选
         left: 'center', // 标题的位置，例如 'left', 'center', 'right' 或者具体像素值
         top: 'top', // 标题的垂直位置，例如 'top', 'middle', 'bottom' 或者具体像素值
         textStyle: { // 标题的文本样式
