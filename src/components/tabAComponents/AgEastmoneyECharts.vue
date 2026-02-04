f2142a2701f927677bcd9844a5809d94ca832b01
xyztai <taixyz@163.com>
2026-02-04 22:02:46 +0800

    Update AgEastmoneyECharts.vue

--- src/components/tabAComponents/AgEastmoneyECharts.vue
+++ src/components/tabAComponents/AgEastmoneyECharts.vue
@@ -265,6 +265,181 @@ onMounted(async () => {
     ], 
   }
 
+  // 配置项
+  const options_2 = {
+    title: {
+        // text: 'S_06 ~ S_09 走势', // 图表的标题内容
+        // subtext: 'S_06 ~ S_09 走势', // 图表的副标题，可选
+        left: 'center', // 标题的位置，例如 'left', 'center', 'right' 或者具体像素值
+        top: 'top', // 标题的垂直位置，例如 'top', 'middle', 'bottom' 或者具体像素值
+        textStyle: { // 标题的文本样式
+            color: '#333', // 字体颜色
+            fontSize: 18 // 字体大小
+        }
+    },
+    xAxis: {
+      // name: '(序号)',
+      type: 'category',
+      data: data.map((item) => item.name),
+      axisTick: {
+        show: true,
+      },
+      axisLabel: {
+        show: false // 不显示刻度标签,一般都是要显示的
+      },
+      boundaryGap: true
+      // axisLabel: {
+      //   interval: 1, // 每隔一个位置显示数值
+      //   formatter: '{value} (刻)',
+      // },
+    },
+    yAxis: {
+      name: '(次)',
+      type: 'value',
+      // axisLabel: {
+      //   formatter: function (value) {
+      //     return value + ' 个'
+      //   }
+      // }
+    }, 
+    tooltip: {
+      trigger: 'axis', // 触发类型为item，表示在圆点上触发显示
+      formatter: '{b}<br/>{a0}: {c0}<br/>{a1}: {c1}<br/>{a2}: {c2}<br/>{a3}: {c3}', // 显示的格式，{b}表示类目值，{c}表示数值
+      textStyle:{
+        align:'left'
+      }
+    },
+    legend: {
+        top: '0%',
+        data: ['S_06','S_07','S_08','S_09']//图例
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    series: [
+      {
+        name: 'S_06',
+        data: data.map((item) => item.value1),
+        type: 'line',
+        markPoint: {
+            data: [
+                {type: 'max', name: '最大值'},
+                {type: 'min', name: '最小值'}
+            ]
+        },
+        symbol: 'circle', // 设置为圆形
+        symbolSize: 6, // 设置圆形的大小
+        color: '#00FF00',//颜色
+        itemStyle: {
+          color: '#00FF00', // 设置圆形的填充颜色
+          borderColor: '#00FF00', // 设置圆形的边框颜色
+        },
+        emphasis: {
+          // 设置选中状态下的样式
+          itemStyle: {
+            borderWidth: 6, // 边框宽度
+            borderColor: '#00FF00', // 边框颜色
+            // shadowBlur: 10, // 阴影模糊度
+            // shadowColor: 'rgba(0, 0, 0, 0.3)' // 阴影颜色
+          }
+        },
+        smooth: true,
+      },
+      {
+        name: 'S_07',
+        data: data.map((item) => item.value2),
+        type: 'line',
+        markPoint: {
+            data: [
+                {type: 'max', name: '最大值'},
+                {type: 'min', name: '最小值'}
+            ]
+        },
+        symbol: 'circle', // 设置为圆形
+        symbolSize: 6, // 设置圆形的大小
+        color: '#0000FF',//颜色
+        itemStyle: {
+          color: '#0000FF', // 设置圆形的填充颜色
+          borderColor: '#0000FF', // 设置圆形的边框颜色
+        },
+        emphasis: {
+          // 设置选中状态下的样式
+          itemStyle: {
+            borderWidth: 6, // 边框宽度
+            borderColor: '#0000FF', // 边框颜色
+            // shadowBlur: 10, // 阴影模糊度
+            // shadowColor: 'rgba(0, 0, 0, 0.3)' // 阴影颜色
+          }
+        },
+        smooth: true,
+      },
+      {
+        name: 'S_08',
+        data: data.map((item) => item.value3),
+        type: 'line',
+        markPoint: {
+            data: [
+                {type: 'max', name: '最大值'},
+                {type: 'min', name: '最小值'}
+            ]
+        },
+        symbol: 'circle', // 设置为圆形
+        symbolSize: 6, // 设置圆形的大小
+        color: '#FFFF00',//颜色
+        itemStyle: {
+          color: '#FFFF00', // 设置圆形的填充颜色
+          borderColor: '#FFFF00', // 设置圆形的边框颜色
+        },
+        emphasis: {
+          // 设置选中状态下的样式
+          itemStyle: {
+            borderWidth: 6, // 边框宽度
+            borderColor: '#FFFF00', // 边框颜色
+            // shadowBlur: 10, // 阴影模糊度
+            // shadowColor: 'rgba(0, 0, 0, 0.3)' // 阴影颜色
+          }
+        },
+        smooth: true,
+      },
+      {
+        name: 'S_09',
+        data: data.map((item) => item.value4),
+        type: 'line',
+        markPoint: {
+            data: [
+                {type: 'max', name: '最大值'},
+                {type: 'min', name: '最小值'}
+            ]
+        },
+        symbol: 'circle', // 设置为圆形
+        symbolSize: 6, // 设置圆形的大小
+        color: '#FF0000',//颜色
+        itemStyle: {
+          color: '#FF0000', // 设置圆形的填充颜色
+          borderColor: '#FF0000', // 设置圆形的边框颜色
+        },
+        emphasis: {
+          // 设置选中状态下的样式
+          itemStyle: {
+            borderWidth: 6, // 边框宽度
+            borderColor: '#FF0000', // 边框颜色
+            // shadowBlur: 10, // 阴影模糊度
+            // shadowColor: 'rgba(0, 0, 0, 0.3)' // 阴影颜色
+          }
+        },
+        smooth: true,
+      }
+    ], 
+  }
+
   // 设置图表配置项
   chartInstance.setOption(options)
   chartInstance_2.setOption(options)
