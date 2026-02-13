10796a5df47b116a7e19409a978fc55b53d6016a
xyztai <taixyz@163.com>
2026-02-13 19:54:22 +0800

    1

--- src/components/tabAComponents/AgEastmoneyECharts.vue
+++ src/components/tabAComponents/AgEastmoneyECharts.vue
@@ -257,7 +257,7 @@ onMounted(async () => {
         type: 'bar',
         markPoint: {
             data: [
-                {type: 'max', name: '最大值', symbolSize: 3},
+                {type: 'max', name: '最大值', symbolSize: 12},
                 // {type: 'min', name: '最小值'}
             ]
         },
