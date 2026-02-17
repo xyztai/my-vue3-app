ff36a1727322ea311d9d29d3faeaf126893cb029
xyztai <taixyz@163.com>
2026-02-18 01:14:47 +0800

    etf-90-days

--- src/components/tabAComponents/AgEastmoneyETF.vue
+++ src/components/tabAComponents/AgEastmoneyETF.vue
@@ -97,6 +97,7 @@ export default {
       { value: 1, label: 'top10-next' },
       { value: 3, label: 'volume*2' },
       // { value: 7, label: 'risk-ratio' },
+      { value: 10, label: '90天内10倍' },
       { value: 8, label: '今日数据' },
       // { value: 4, label: '1.5%-2020' },
       // { value: 5, label: 'QQ-1.5%-2024919' }
@@ -229,6 +230,24 @@ export default {
 
         if(selectedValue.value == 8) {
           const method = 'eastmoney-latest-info';
+          const key = 'etf-' + method;
+          const myData = getCachedData(key);
+          if (!myData) {
+            // 从API获取数据并缓存
+            const response = await axios.get('/ag-eastmoney-etf/' + method )
+            // console.log('response.data.data========', response.data.data)
+            tableData.value = response.data.data
+            // console.log('tableData.value========', tableData.value)          
+            cacheData(key, response.data.data)
+          } else {
+            // 使用缓存的数据
+            tableData.value = myData
+            // console.log('Using cached data:', myData);
+          }
+        }
+
+        if(selectedValue.value == 10) {
+          const method = 'etf-90-days';
           const key = 'etf-' + method;
           const myData = getCachedData(key);
           if (!myData) {
