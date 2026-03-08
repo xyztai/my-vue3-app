d02ad1d036da1d7f0065cae1f5536fbcdd852892
xyztai <taixyz@163.com>
2026-03-08 13:18:07 +0800

    1

--- src/components/tabAComponents/AgEastmoneyETF.vue
+++ src/components/tabAComponents/AgEastmoneyETF.vue
@@ -91,8 +91,8 @@ export default {
     // const inputValue4 = ref(2)
     const selectedValue = ref(0) // 下拉框选中的值
     const options = ref([ // 下拉框选项数据
-      { value: 0, label: '*chg-top3' },
-      { value: 11, label: 'chg-top3-history' },
+      { value: 11, label: '*chg-top3' },
+      { value: 12, label: 'chg-top3-history' },
       { value: 1, label: 'top10-next' },
       { value: 2, label: 'top10-60' },
       { value: 3, label: 'volume*2' },
@@ -122,7 +122,7 @@ export default {
           const responseFirstApi = await axios.get('/ag-new/invalidateAll');
         }
 
-        if(selectedValue.value == 0) {
+        if(selectedValue.value == 11) {
           const method = 'etf-chg-top3';
           const key = 'etf-' + method;
           const myData = getCachedData(key);
@@ -140,7 +140,7 @@ export default {
           }          
         }
 
-        if(selectedValue.value == 11) {
+        if(selectedValue.value == 12) {
           const method = 'etf-chg-top3-history';
           const key = 'etf-' + method;
           const myData = getCachedData(key);
