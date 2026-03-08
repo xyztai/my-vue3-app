5209d2c37a7e6fe59816e6a76fc6e38fbaf27ea2
xyztai <taixyz@163.com>
2026-03-08 13:22:25 +0800

    1

--- src/components/tabAComponents/AgEastmoneyETF.vue
+++ src/components/tabAComponents/AgEastmoneyETF.vue
@@ -89,7 +89,7 @@ export default {
     const inputValue2 = ref('-1')
     const inputValue3 = ref('300')
     // const inputValue4 = ref(2)
-    const selectedValue = ref(0) // 下拉框选中的值
+    const selectedValue = ref(11) // 下拉框选中的值
     const options = ref([ // 下拉框选项数据
       { value: 11, label: '*chg-top3' },
       { value: 12, label: 'chg-top3-history' },
