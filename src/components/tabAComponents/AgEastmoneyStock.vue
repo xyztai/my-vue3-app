69a69709c04f21548b5eb3cafa972265446830b8
xyztai <taixyz@163.com>
2026-04-18 20:46:08 +0800

    1

--- src/components/tabAComponents/AgEastmoneyStock.vue
+++ src/components/tabAComponents/AgEastmoneyStock.vue
@@ -91,8 +91,9 @@ export default {
     const inputValue2 = ref('-1')
     const inputValue3 = ref('300')
     // const inputValue4 = ref(2)
-    const selectedValue = ref(20) // 下拉框选中的值
+    const selectedValue = ref(21) // 下拉框选中的值
     const options = ref([ // 下拉框选项数据
+      { value: 21, label: '*必买-分5次加-每次1w' },
       { value: 20, label: '*ssp-最近10个交易日有冲高-然后均线粘结' },
       { value: 19, label: '*跳空高开，等回落' },
       { value: 18, label: '*截图-2025-3倍以上' },
@@ -441,6 +442,24 @@ export default {
 
         if(selectedValue.value == 20) {
           const method = 'eastmoney-MA20maSSP';
+          const key = 'stock-' + method;
+          const myData = getCachedData(key);
+          if (!myData) {
+            // 从API获取数据并缓存
+            const response = await axios.get('/ag-eastmoney-stock/' + method )
+            // console.log('response.data.data========', response.data.data)
+            tableData.value = response.data.data
+            // console.log('tableData.value========', tableData.value)          
+            cacheData(key, response.data.data)
+          } else {
+            // 使用缓存的数据
+            tableData.value = myData
+            // console.log('Using cached data:', myData);
+          }
+        }
+
+        if(selectedValue.value == 21) {
+          const method = 'eastmoney-considerAll';
           const key = 'stock-' + method;
           const myData = getCachedData(key);
           if (!myData) {
