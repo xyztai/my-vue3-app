e9fc23266e75c8fd064f66aa73d539ecf705b2d1
xyztai <taixyz@163.com>
2026-05-27 07:18:13 +0800

    1

--- src/components/tabAComponents/AgEastmoneyMust.vue
+++ src/components/tabAComponents/AgEastmoneyMust.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <p>买:</p>    
-    <p>boll下轨 + KDJ底背离、金叉 + 最好缩量</p>
+    <div class="text-container">
+    <p>买:boll下轨 + KDJ底背离、金叉 + 最好缩量</p>
+    </div>
     <div>
       <img src="/buy-01.PNG" width="410" alt="实例-百度">
     </div>
     <div>
       <img src="/buy-02.PNG" width="410" alt="实例-百度">
     </div>
-    <p>卖:</p>    
-    <p>boll上轨 + KDJ顶背离、死叉</p>
+    <div class="text-container">
+    <p>卖:boll上轨 + KDJ顶背离、死叉</p>
+    </div>
     <div>
       <img src="/sell-01.PNG" width="410" alt="实例-百度">
     </div>
