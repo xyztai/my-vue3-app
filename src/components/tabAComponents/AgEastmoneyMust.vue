3bd95313c15b7c40959ff004699a3a9a6ff042e8
xyztai <taixyz@163.com>
2026-05-27 06:58:29 +0800

    1

--- src/components/tabAComponents/AgEastmoneyMust.vue
+++ src/components/tabAComponents/AgEastmoneyMust.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <img src="/baidu.png" alt="实例-百度">
+      <img src="/baidu.png"  width="410" alt="实例-百度">
     </div>
     <div class="text-container">
       <p>最重要:</p>
