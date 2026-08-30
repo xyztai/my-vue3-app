efa3eda2488a20b7a1ae833a2a51f7b54e9dccb4
xyztai <taixyz@163.com>
2026-08-30 21:40:35 +0800

    1

--- src/components/tabAComponents/AgEastmoneyStockStrategy.vue
+++ src/components/tabAComponents/AgEastmoneyStockStrategy.vue
@@ -255,8 +255,8 @@ export default {
       if(str.startsWith("当天chg=")) {
         return '当天chg=';
       }
-      const reg = new RegExp(targetWord, 'g')
-      return originalText.value.replace(reg, `<span style="color: green;">${targetWord}</span>`);
+      const reg = new RegExp('---', 'g')
+      return str.value.replace(reg, `<span style="color: green;">${targetWord}</span>`);
       // return str;
     },
     // 正值
